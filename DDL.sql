48134be79255c05bc68c1fe02c81ea427386819b
rauldmennys <37989895+rauldmennys@users.noreply.github.com>
2026-03-15 18:07:41 -0600

    Add files via upload
    Creacion de un objeto tipo tabla en SQL Server.

---
+++ DDL.sql
@@ -0,0 +1,19 @@
+CREATE TABLE RegistroAcademicoInicial
+(
+    Carnet VARCHAR(20),
+    Nombres VARCHAR(150),
+    Sexo VARCHAR(20),
+    FechaNac DATE,
+    Departamento VARCHAR(100),
+    Municipio VARCHAR(100),
+    Email VARCHAR(150),
+    Direccion VARCHAR(250),
+    Facultad VARCHAR(150),
+    Carrera VARCHAR(150),
+    Semestre VARCHAR(10),
+    Turno VARCHAR(20),
+    Asignatura VARCHAR(150),
+    Anio INT,
+    NombreProfesor VARCHAR(150),
+    NivelAcadProf VARCHAR(50)
+);
